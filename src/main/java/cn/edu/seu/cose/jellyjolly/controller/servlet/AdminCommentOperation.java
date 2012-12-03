b96fc803a0e89875b446314f3d31e45365f67fb2
Wenhao Ji <predator.ray@gmail.com>
2012-12-03 16:55:30 +0800

    代码样式调整

--- src/main/java/cn/edu/seu/cose/jellyjolly/controller/servlet/AdminCommentOperation.java
+++ src/main/java/cn/edu/seu/cose/jellyjolly/controller/servlet/AdminCommentOperation.java
@@ -29,7 +29,6 @@ import java.util.logging.Logger;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
