96877e68d09ef18b5aea2d7d3112bd5b02413f80
Wenhao Ji <predator.ray@gmail.com>
2012-12-05 11:50:42 +0800

    修正代码样式，修正一个配置文件引用错误

--- src/main/java/cn/edu/seu/cose/jellyjolly/controller/BlogPostController.java
+++ src/main/java/cn/edu/seu/cose/jellyjolly/controller/BlogPostController.java
@@ -4,16 +4,9 @@
  */
 package cn.edu.seu.cose.jellyjolly.controller;
 
-import cn.edu.seu.cose.jellyjolly.dao.BlogInfoDataAccess;
-import cn.edu.seu.cose.jellyjolly.dao.BlogPageDataAccess;
 import cn.edu.seu.cose.jellyjolly.dao.BlogPostDataAccess;
-import cn.edu.seu.cose.jellyjolly.dao.CategoryDataAccess;
 import cn.edu.seu.cose.jellyjolly.dao.DataAccessException;
-import cn.edu.seu.cose.jellyjolly.dto.BlogInfo;
-import cn.edu.seu.cose.jellyjolly.dto.BlogPageBar;
 import cn.edu.seu.cose.jellyjolly.dto.BlogPost;
-import cn.edu.seu.cose.jellyjolly.dto.Category;
-import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
