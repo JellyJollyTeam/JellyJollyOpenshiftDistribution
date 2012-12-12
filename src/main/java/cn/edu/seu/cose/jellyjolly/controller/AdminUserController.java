0a9bbae139e57f0027da0df804d0d5d2674831b8
rAy <predator.ray@gmail.com>
2012-12-12 14:40:36 +0800

    实现AdminUserController

--- src/main/java/cn/edu/seu/cose/jellyjolly/controller/AdminUserController.java
+++ src/main/java/cn/edu/seu/cose/jellyjolly/controller/AdminUserController.java
@@ -5,7 +5,14 @@
 package cn.edu.seu.cose.jellyjolly.controller;
 
 import cn.edu.seu.cose.jellyjolly.dao.AdminUserDataAccess;
+import cn.edu.seu.cose.jellyjolly.dao.DataAccessException;
+import cn.edu.seu.cose.jellyjolly.dto.AdminUser;
+import cn.edu.seu.cose.jellyjolly.model.session.UserAuthorization;
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,15 +24,109 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AdminUserController {
 
+    public static final String SESSION_ATTRI_AUTH = "userAuth";
+    private static final long THIRTY_MINUTES_MILLIS = 1000 * 60 * 30;
     private AdminUserDataAccess adminUserDataAccess;
+    private long expireDelta = THIRTY_MINUTES_MILLIS;
 
     public AdminUserController(AdminUserDataAccess adminUserDataAccess) {
         this.adminUserDataAccess = adminUserDataAccess;
     }
 
-    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public void setExpireDelta(long expireDelta) {
+        this.expireDelta = expireDelta;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String logIn() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String logIn(@RequestParam String username,
-            @RequestParam String password) {
-        return "redirect:";
+            @RequestParam String password, HttpServletRequest request)
+            throws DataAccessException {
+        AdminUser userConfirmed =
+                adminUserDataAccess.getUserIfConfirmed(username, password);
+        if (userConfirmed == null) {
+            return "redirect:/login";
+        }
+        HttpSession currentSession = request.getSession();
+        long currentTimeMillis = System.currentTimeMillis();
+        long expireTime = currentTimeMillis + expireDelta;
+        UserAuthorization userAuth = new UserAuthorization(userConfirmed,
+                currentTimeMillis, expireTime);
+        currentSession.setAttribute(SESSION_ATTRI_AUTH, userAuth);
+        return "redirect:/admin";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.PUT)
+    public String addNewAdminUser(@RequestParam String username,
+            @RequestParam String password, @RequestParam String email,
+            @RequestParam String homePage) throws DataAccessException {
+        Date currentDate = new Date();
+        adminUserDataAccess.addNewUser(username, password, email, username,
+                homePage, currentDate);
+        return "redirect:/admin/admins";
+    }
+
+    @RequestMapping(value = "/admin/{userId}", method = RequestMethod.DELETE)
+    public String deleteAdminUser(@PathVariable long userId)
+            throws DataAccessException {
+        adminUserDataAccess.deleteUser(userId);
+        return "redirect:/admin/admins";
+    }
+
+    @RequestMapping(value = "/admin/{userId}/username",
+    method = RequestMethod.PUT)
+    public String changeUsername(@PathVariable long userId,
+            @RequestParam String username) throws DataAccessException {
+        adminUserDataAccess.changeUserName(userId, username);
+        return "redirect:/admin/admins";
+    }
+
+    @RequestMapping(value = "/admin/{userId}/password",
+    method = RequestMethod.PUT)
+    public String changePassword(@PathVariable long userId,
+            @RequestParam String oldPassword, @RequestParam String newPassword)
+            throws DataAccessException {
+        AdminUser userToBeConfirmed = adminUserDataAccess.getUser(userId);
+        String username = userToBeConfirmed.getUsername();
+        boolean confirmed = adminUserDataAccess.confirm(username, oldPassword);
+        if (!confirmed) {
+            return "redirect:/admin/admins/" + userId + "?err=1";
+        }
+        adminUserDataAccess.changePassword(userId, newPassword);
+        return "redirect:/admin/admins" + userId;
+    }
+
+    @RequestMapping(value = "/admin/{userId}/displayName",
+    method = RequestMethod.PUT)
+    public String changeDisplayName(@PathVariable long userId,
+            @RequestParam String displayName) throws DataAccessException {
+        AdminUser userToBeUpdated = adminUserDataAccess.getUser(userId);
+        userToBeUpdated.setDisplayName(displayName);
+        adminUserDataAccess.updateUser(userToBeUpdated);
+        return "redirect:/admin/admins" + userId;
+    }
+
+    @RequestMapping(value = "/admin/{userId}/email",
+    method = RequestMethod.PUT)
+    public String changeEmail(@PathVariable long userId,
+            @RequestParam String email) throws DataAccessException {
+        AdminUser userToBeUpdated = adminUserDataAccess.getUser(userId);
+        userToBeUpdated.setEmail(email);
+        adminUserDataAccess.updateUser(userToBeUpdated);
+        return "redirect:/admin/admins" + userId;
+    }
+
+    @RequestMapping(value = "/admin/{userId}/homePage",
+    method = RequestMethod.PUT)
+    public String changeHomePage(@PathVariable long userId,
+            @RequestParam String homePage) throws DataAccessException {
+        AdminUser userToBeUpdated = adminUserDataAccess.getUser(userId);
+        userToBeUpdated.setHomePageUrl(homePage);
+        adminUserDataAccess.updateUser(userToBeUpdated);
+        return "redirect:/admin/admins" + userId;
     }
 }
