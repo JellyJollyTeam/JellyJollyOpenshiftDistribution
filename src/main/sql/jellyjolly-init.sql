1574cc21aac149f0fc645941c1e609c333ac2184
Ray <Ray@192.168.0.100>
2013-01-17 15:44:31 +0800

    sql初始化脚本添加一个默认分类；最后一个分类不会自动删除

--- src/main/sql/jellyjolly-init.sql
+++ src/main/sql/jellyjolly-init.sql
@@ -2,3 +2,7 @@ INSERT INTO `jellyjolly_schema`.`jj_blog_info`
 (blog_title, blog_subtitle)
 VALUES ('我的博客', '使用JellyJolly')
 ;
+
+INSERT INTO `jellyjolly_schema`.`jj_categories`
+(name) VALUES ('默认')
+;
