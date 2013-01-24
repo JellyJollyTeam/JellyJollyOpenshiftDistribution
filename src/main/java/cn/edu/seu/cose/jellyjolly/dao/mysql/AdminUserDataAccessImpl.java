820015b30640f8f193a2a2588286b3f0754ac112
Ray <Ray@192.168.0.100>
2013-01-24 15:51:46 +0800

    修改mysql实现包名

--- src/main/java/cn/edu/seu/cose/jellyjolly/dao/mysql/AdminUserDataAccessImpl.java
+++ src/main/java/cn/edu/seu/cose/jellyjolly/dao/mysql/AdminUserDataAccessImpl.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package cn.edu.seu.cose.jellyjolly.dao.jdbc;
+package cn.edu.seu.cose.jellyjolly.dao.mysql;
 
 import cn.edu.seu.cose.jellyjolly.dao.AdminUserDataAccess;
 import cn.edu.seu.cose.jellyjolly.dao.DataAccessException;
